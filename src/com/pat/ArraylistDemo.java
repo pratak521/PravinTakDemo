56ddb7c73a987738cc28178f8024455d5038a226
Pravin Tak <pratak521@gmail.com>
2020-06-29 11:54:25 +0530

    This is for demo purpose

---
+++ src/com/pat/ArraylistDemo.java
@@ -0,0 +1,23 @@
+package com.pat;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArraylistDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		ArrayList<Integer> al=new ArrayList<>();
+		Scanner scan=new Scanner(System.in);
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Enter the number");
+			al.add(scan.nextInt());
+		}
+		al.forEach(n->System.out.println(n));
+		System.out.println(al);
+	
+	}
+
+}
